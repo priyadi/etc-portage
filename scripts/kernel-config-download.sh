18bedc18f6c9f55d6b1bccd377e54c61b429c0e5
Priyadi Iman Nurcahyo <1102197+priyadi@users.noreply.github.com>
2021-02-04 12:12:54 +0700

    update fedora url

--- scripts/kernel-config-download.sh
+++ scripts/kernel-config-download.sh
@@ -3,7 +3,7 @@
 CONFIG=/usr/src/kernel-x86_64-fedora.config
 DOTCONFIG=/usr/src/linux/.config
 ADDITIONAL=/etc/portage/kernel.config.additional
-URL=https://src.fedoraproject.org/rpms/kernel/raw/master/f/kernel-x86_64-fedora.config
+URL=https://src.fedoraproject.org/rpms/kernel/raw/rawhide/f/kernel-x86_64-fedora.config
 
 wget $URL -O $CONFIG
 cp $CONFIG $DOTCONFIG
