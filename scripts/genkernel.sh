9dd36475e9f00d6ef000559deafbb7206cb9e8aa
Priyadi Iman Nurcahyo <1102197+priyadi@users.noreply.github.com>
2021-02-16 00:51:53 +0700

    stuff

--- scripts/genkernel.sh
+++ scripts/genkernel.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 
-date=`date +%Y-%m-%d`
+DATE=`date +%Y-%m-%d`
+CONFIG=/usr/src/kernel-x86_64-fedora.config
+DOTCONFIG=/usr/src/linux/.config
+ADDITIONAL=/etc/portage/kernel.config.additional
+URL=https://src.fedoraproject.org/rpms/kernel/raw/rawhide/f/kernel-x86_64-fedora.config
+
+wget $URL -O $CONFIG
+cp $CONFIG $DOTCONFIG
+cat $ADDITIONAL >> $DOTCONFIG
 genkernel \
-	--initramfs-filename=initramfs-$date-%%KV%%.img \
-	--kernel-filename=vmlinuz-$date-%%KV%% \
-	--systemmap-filename=System.map-$date-%%KV%% \
+	--initramfs-filename=initramfs-$DATE-%%KV%%.img \
+	--kernel-filename=vmlinuz-$DATE-%%KV%% \
+	--systemmap-filename=System.map-$DATE-%%KV%% \
 	all
