d072d702fa734527f996ad57fc5a8df309a92f33
Priyadi Iman Nurcahyo <1102197+priyadi@users.noreply.github.com>
2022-01-10 13:20:34 +0700

    emerge.sh

--- scripts/emerge.sh
+++ scripts/emerge.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 sudo eix-sync
-sudo emerge -DuNav world --keep-going --backtrack 30 && sudo emerge --depclean
-sudo eclean distfiles
-sudo eclean-kernel
+sudo emerge -DuNav world --keep-going --backtrack 30 \
+	&& sudo emerge --depclean \
+	&& sudo eclean distfiles \
+	&& sudo eclean-kernel
