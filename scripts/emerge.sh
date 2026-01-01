de32c13b6d9854e849769383433b4afe4fe7555a
Priyadi Iman Nurcahyo <1102197+priyadi@users.noreply.github.com>
2026-01-01 16:25:26 +0700

    stuff

--- scripts/emerge.sh
+++ scripts/emerge.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# sudo systemd-run --pty --wait   --nice=19   -p CPUQuota=600% -p CPUWeight=5 -p IOWeight=5   -p IOSchedulingClass=idle   bash -lc 'emerge -DuNav world --keep-going'
+
+
 sudo emerge --sync
 sudo eix-update
 sudo emerge -DuNav world --keep-going --backtrack 30 --verbose-conflicts \
